30ce73b9f8252b37c3b22b3af3ef56ffe92719ec
kuldeepsengar01 <kuldeepsengar8218@gmail.com>
2026-09-15 14:58:31 +0530

    Add Certificate item to Navbar

--- frontend/Ksportfolio/src/component/Navbar.jsx
+++ frontend/Ksportfolio/src/component/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
     { name: "Home", path: "/" },
     { name: "About", path: "/about" },
     { name: "Skills", path: "/skills" },
+    { name: "Certificate", path: "/certificate" },
     { name: "Projects", path: "/projects" },
     { name: "Contact", path: "/contact" },
   ];
